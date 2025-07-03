d2bd379ab052755f7ee67bca02a9330e21cca6ac
thevivethakur <121496427+thevivethakur@users.noreply.github.com>
2025-07-04 01:19:05 +0530

    Add files via upload

---
+++ entities.js
@@ -0,0 +1,19 @@
+import { base44 } from './base44Client';
+
+
+export const Inspection = base44.entities.Inspection;
+
+export const Shipment = base44.entities.Shipment;
+
+export const Document = base44.entities.Document;
+
+export const FactoryAudit = base44.entities.FactoryAudit;
+
+export const SampleInspection = base44.entities.SampleInspection;
+
+export const QCForgeProject = base44.entities.QCForgeProject;
+
+
+
+// auth sdk:
+export const User = base44.auth;
